2da0003477214c96b5483d05d97abc4c13f4d426
FranzPrevisionz <franz.schramm@previsionz.de>
2020-09-14 13:40:13 +0000

    fixed incorrect secret return

--- kv_secrets.py
+++ kv_secrets.py
@@ -3,14 +3,14 @@ import cmd
 from azure.keyvault.secrets import SecretClient
 from azure.identity import DefaultAzureCredential
 
-get_bearer_token():
+def get_bearer_token():
     keyVaultName = os.environ["KEY_VAULT_NAME"]
     KVUri = "https://{}.vault.azure.net".format(keyVaultName)
     secretName = "BEARER-TOKEN"
     credential = DefaultAzureCredential()
     client = SecretClient(vault_url=KVUri, credential=credential)
     print("Retrieving your secret from {}.".format(keyVaultName))
-    retrieved_secret = client.get_secret(secretName)
-    print("Your secret is '{}'.".format(retrieved_secret.value))
-    print(" done.")
+    retrieved_secret = client.get_secret(secretName).value
+    print(retrieved_secret)
+    print("Secret retrieval done.")
     return retrieved_secret
